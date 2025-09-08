fad9a73ca69f3959663062fd2e1b043811e556d0
Matheus Luiz Soares <matheusluiz15a@gmail.com>
2025-09-07 22:41:35 -0300

    criando um app novo site setup e criando os models para admin django

--- djangoapp/project/settings.py
+++ djangoapp/project/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "blog",
-    'django_summernote'
+    'django_summernote',
+    'site_setup',
 ]
 
 MIDDLEWARE = [
