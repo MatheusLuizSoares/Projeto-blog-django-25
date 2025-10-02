a317d91df2fa28c7826be66dd27626b20da9ef76
Matheus Luiz Soares <matheusluiz15a@gmail.com>
2025-10-02 15:27:28 -0300

    adicionei context_processors.py

--- djangoapp/project/settings.py
+++ djangoapp/project/settings.py
@@ -69,6 +69,7 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
                 'site_setup.context_prcessors.context_processor_example',
+                'site_setup.context_prcessors.site_setup',
             ],
         },
     },
